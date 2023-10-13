127792608dd815fa838dce58505a62284f898265
JustinFourie1993 <>
2023-10-13 10:59:41 +0000

    add MealList class view, base and index templates and url for index

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Meal
 
-# Create your views here.
+
+class MealList(generic.ListView):
+    model = Meal
+    queryset = Meal.objects.order_by('price')
+    template_name = 'index.html'
