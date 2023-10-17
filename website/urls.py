fb1378db76b4ba5c32ecd6e2c01f6f57d004f206
JustinFourie1993 <>
2023-10-17 09:49:27 +0000

    add view, url, structure, css and images for index.html

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.MealList.as_view(), name='menu')
+    # path('', views.MealList.as_view(), name='menu'),
+    path('', views.index, name='index'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,7 +3,11 @@ from django.views import generic
 from .models import Meal
 
 
-class MealList(generic.ListView):
-    model = Meal
-    queryset = Meal.objects.order_by('price')
-    template_name = 'menu.html'
+# class MealList(generic.ListView):
+#     model = Meal
+#     queryset = Meal.objects.order_by('price')
+#     template_name = 'menu.html'
+
+
+def index(request):
+    return render(request, 'index.html')
