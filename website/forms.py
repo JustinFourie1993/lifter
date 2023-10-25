08676d938c78bbf01a69e7edc6f796a2d00346d4
JustinFourie1993 <>
2023-10-25 14:31:15 +0000

    add widgets to booking form

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,13 @@ from django import forms
 
 # form created from booking model
 
+
 class MakeBooking(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['date','time','party_of']
+        fields = ['date', 'time', 'party_of']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'party_of': forms.NumberInput(attrs={'type': 'number', 'min': '1'})
+        }
