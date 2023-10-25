d7647ee1d60cc7b208df4ee419fac748f7e45957
JustinFourie1993 <>
2023-10-25 10:10:38 +0000

    feat: add path for edit_booking view

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('menu/', views.MealList.as_view(), name='menu'),
     path('', views.index, name='home'),
     path('booking/', views.make_booking, name='booking'),
+    path('booking/edit/<int:booking_id>/',
+         views.edit_booking, name='edit_booking'),
 ]
