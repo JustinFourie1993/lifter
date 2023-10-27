f69681c84446b84e7a6dda7beeed89c79477fb59
JustinFourie1993 <>
2023-10-27 11:42:33 +0000

    feat: add test_make_booking_form_no_data function

--- website/test_forms.py
+++ website/test_forms.py
@@ -4,10 +4,15 @@ from .forms import MakeBooking
 
 class MakeBookingFormTest(TestCase):
 
-    def test_make_booking_form_is_valid_(self):
+    def test_make_booking_form_is_valid(self):
         form = MakeBooking(data={
             'date': '2022-10-25',
             'time': '14:00',
             'party_of': 4
         })
         self.assertTrue(form.is_valid())
+
+    def test_make_booking_form_no_data(self):
+        form = MakeBooking(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 3)
